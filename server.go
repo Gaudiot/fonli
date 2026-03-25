905b380ce08367e42f01b57d7d72b0f984cdd503
Victor Gaudiot <victor.gaudiot@uber.com>
2026-03-25 15:48:30 -0300

    auth router

--- server.go
+++ server.go
@@ -4,7 +4,12 @@ import (
 	"log"
 	"net/http"
 
+	refreshtoken_repo "gaudiot.com/fonli/base/repositories/refresh_token"
+	user_repo "gaudiot.com/fonli/base/repositories/user"
 	"gaudiot.com/fonli/core"
+	"gaudiot.com/fonli/core/security/password"
+	"gaudiot.com/fonli/core/security/tokens"
+	"gaudiot.com/fonli/src/authentication"
 	storytranslation "gaudiot.com/fonli/src/story_translation"
 	wordconjugationexercise "gaudiot.com/fonli/src/word_conjugation"
 	wordtranslationexercise "gaudiot.com/fonli/src/word_translation"
@@ -19,7 +24,16 @@ func main() {
 
 	envConfig := core.GetEnvConfig()
 
+	// TODO: replace mocks with real repository implementations
+	tokenService := tokens.NewTokenService([]byte(envConfig.JWTSecret))
+	passwordService := &password.BCryptPasswordService{}
+	userRepository := &user_repo.UserRepositoryMock{Users: make(map[string]*user_repo.User)}
+	refreshTokenRepository := &refreshtoken_repo.RefreshTokenRepositoryMock{RefreshTokens: make(map[string]*refreshtoken_repo.RefreshToken)}
+	authService := authentication.NewAuthService(*tokenService, passwordService, userRepository, refreshTokenRepository)
+
 	router := chi.NewRouter()
+	router.Route("/auth", authentication.AuthenticationRouter(authService))
+
 	router.Route("/word-translation", wordtranslationexercise.WordTranslationRouter)
 	router.Route("/word-conjugation", wordconjugationexercise.WordConjugationRouter)
 	router.Route("/history-translation", storytranslation.StoryTranslationRouter)
